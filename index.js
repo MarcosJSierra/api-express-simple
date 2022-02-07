fcd0ca7c5e65afee864b04edc9868f51d53d6005
Marcos Sierra <marcosjspac@gmail.com>
2022-02-07 02:34:35 -0600

    agregando petición de prueba a nuestra API

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+
+app.get("/", (req, res) => res.send(
+    "<h1>API Simple con Express</h1>"
+));
+
 app.listen(5000, () => {
-    console.log("Servidor escuchando al puerto")
+    console.log("Servidor escuchando al puerto 5000")
 });
+
+
+export { app }
